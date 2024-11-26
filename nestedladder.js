11a9a3d1bfece6c17f44ca999f38f12d51a23cf1
nencymaisuriya <160009308+nencymaisuriya@users.noreply.github.com>
2024-11-26 18:27:35 +0530

    Add files via upload

---
+++ nestedladder.js
@@ -0,0 +1,71 @@
+//opreators 
+//nested if else ladder if else
+//1.nested if else 
+/* 
+if(cond){
+    if(cond){
+    statement
+    }else{
+    statement
+    }
+}
+else{
+statement
+}
+*/
+//examples
+//1 check small no using three variable.
+// let a=22,b=4,c=9;
+// if(a<b){
+//     if(a<c){
+//         console.log("small no is",a);
+//     }else{
+//         console.log("smallest ni is",c);
+//     }
+// }else{
+//     console.log("smaller no is",b);
+// }
+
+//2 check big no using three variable.
+// let a=44,b=41,c=70;
+// if(a>b){
+//     if(a>c){
+//         console.log("big no is",a);
+//     }else{
+//         console.log("bigger no is",c);
+//     }
+// }else{
+//     console.log("bigest no is",b);
+// }
+
+//2 ladder if else
+/*
+if(cond){
+ state
+}else if(){
+ state
+}else{
+ state
+}
+*/
+//examples
+//1 check small no using three variable.
+//  let a=22,b=445,c=91;
+//  if(a<b && a<c){
+//     console.log("small no is",a);
+//  }else if(b<c){
+//     console.log("smaller no is",b);
+//  }else{
+//     console.log("smallest no is",c);
+//  }
+//2 check big no using three variable.
+// let a=44,b=411,c=70;
+// if(a>b && a>c){
+//     console.log("big no is",a);
+// }else if(b>c){
+//     console.log("bigger no is",b);
+// }else{
+//     console.log("biggest no is",c);
+// }
+
+
